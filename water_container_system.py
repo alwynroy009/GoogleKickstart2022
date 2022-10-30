791a1eac5752749e507562bae540cbffd165bb26
Alwyn Roy Miranda <75134499+alwynroy009@users.noreply.github.com>
2022-10-30 15:28:21 +0530

    Update water_container_system.py

--- water_container_system.py
+++ water_container_system.py
@@ -1,35 +1,35 @@
 
 # Water Container System
 # https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb409/0000000000bef79e
-# Part 1 - https://youtu.be/xo4Tbs6JABQ
-# part 2 - 
+# (Problem Explanation) Part 1 - https://youtu.be/xo4Tbs6JABQ
+# (Code Explanation) part 2 - https://youtu.be/5uu6FCcg-Qc
 
-for tests in range(1,int(input())+1):
-    N, Q = map(int, input().split())
+for tests in range(1, int(input()) + 1):
+    N, Q  = map(int, input().split())
     connections = {i:set() for i in range(1, N+1)}
     for _ in range(N-1):
-        i, j = map(int, input().split())
+        i , j = map(int, input().split())
         connections[i].add(j)
         connections[j].add(i)
-    
+    # print(connections)
     for _ in range(Q):
         input()
     checked = {1}
     level = [1]
-    levels = []
-
+    capacity = []
     while level:
-        levels.append(len(level)) 
+        capacity.append(len(level)) 
         new_L = []
-        for i in level: 
-            for j in connections[i]: 
-                if j in checked: 
+        for i in level:
+            for j in connections[i]:
+                if j in checked:
                     continue
                 checked.add(j)
                 new_L.append(j)
-        level = new_L  
+            level = new_L
+    # print(capacity)
     result = 0
-    for c in levels:
+    for c in capacity:
         if result+c > Q:
             break
         result += c
