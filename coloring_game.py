e99655e2b2756a9926f40fd58400b4a7488d9165
Alwyn Roy Miranda <75134499+alwynroy009@users.noreply.github.com>
2022-11-10 21:20:09 +0530

    Create coloring_game.py

---
+++ coloring_game.py
@@ -0,0 +1,8 @@
+# Problem Statement
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb0f5/0000000000ba856a
+  
+# Explanation
+# https://youtu.be/sQBS9geDrIA
+
+for tests in range(1,int(input())+1):
+    print(f'Case #{tests}: {((int(input())-1)//5)+1}')
