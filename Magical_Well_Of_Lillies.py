36d234d84521a14af8ece67604318908c91362f4
Alwyn Roy Miranda <75134499+alwynroy009@users.noreply.github.com>
2022-11-15 17:18:54 +0530

    Create Magical_Well_Of_Lillies.py

---
+++ Magical_Well_Of_Lillies.py
@@ -0,0 +1,17 @@
+####### Problem Statement
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb1b6/0000000000c47e79
+
+####### Explanation
+# Work in Progress
+
+L = 10**5
+Ans = list(range(L+2))
+for i in range(5, L+1):
+    Ans[i+1] = min(Ans[i]+1, Ans[i+1])
+    curr = Ans[i] + 4
+    for j in range(2*i, L+1, i):
+        curr += 2
+        Ans[j] = min(Ans[j], curr)
+
+for tests in range(1,int(input())+1):
+    print(f'Case #{tests}: {Ans[int(input())]}')
