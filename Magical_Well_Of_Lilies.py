08fb296de49026a6d4cda2e170140d4eab0cd282
Alwyn Roy Miranda <75134499+alwynroy009@users.noreply.github.com>
2022-11-15 18:45:38 +0530

    Update Magical_Well_Of_Lilies.py

--- Magical_Well_Of_Lilies.py
+++ Magical_Well_Of_Lilies.py
@@ -2,7 +2,7 @@
 # https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb1b6/0000000000c47e79
 
 ####### Explanation
-# https://youtu.be/8SvnF_NdjUA
+# https://youtu.be/SfuzGFwMwT4
 
 L = 10**5
 Ans = list(range(L+2))
